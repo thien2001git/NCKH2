9b386d3cc0bcfd1de13352dbbdcc3668960fd5cb
thien2001git <hoangthien66771508@gmail.com>
2022-04-06 08:50:18 +0700

    create model

--- chat_bot/chinh/admin.py
+++ chat_bot/chinh/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from chinh.models import Images, Usr
+from chinh.models import Cau, Images, Label, Usr
 
 # Register your models here.
 admin.site.register(Images)
 admin.site.register(Usr)
+admin.site.register(Label)
+admin.site.register(Cau)
